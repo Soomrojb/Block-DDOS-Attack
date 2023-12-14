ba85419765b73f8d2624237f24e160253964ea0d
Janib Soomro <soomrojb@gmail.com>
2023-12-15 00:00:27 +0500

    Main Shell Script

---
+++ blockddos.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# add all apache log files in the array
+accesslogs=("/var/log/apache2/websites/website1.com/access.log" "/var/log/apache2/websites/website1.com/access.log")
+
+# add ther terms for searching
+srchterms=("/xmlrpc.php" "/cjfuns.php" "/woh.php" "/xxl.php")
+
+for log in ${accesslogs[@]}; do
+    for trm in ${srchterms[@]}; do
+        cmd=$(tail -n 100 $log | grep "$trm" | cut -d " " -f 1 | xargs -I {} sudo ufw insert 1 deny from {})
+        echo $cmd
+    done
+done
